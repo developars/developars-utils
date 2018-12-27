f7f6229b45ce89cae688a8dcc68919848a82a01c
Carlos Azaustre <cazaustre@gmail.com>
2018-12-28 00:00:53 +0100

    terminate

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
+const terminate = require('./terminate')
 const { getLogger, logHandler } = require('./lib/logger')
 
 module.exports = {
+  terminate,
   getLogger,
   logHandler
 }
