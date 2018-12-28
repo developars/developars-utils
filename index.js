9aebb271daa1a21180551fbc0a4f4da5858bdfd8
Carlos Azaustre <cazaustre@gmail.com>
2018-12-28 09:30:21 +0100

    cache

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict'
 
-const terminate = require('./terminate')
+const terminate = require('./lib/`terminate')
+const cache = require('./lib/cache')
 const { getLogger, logHandler } = require('./lib/logger')
 
 module.exports = {
   terminate,
+  cache,
   getLogger,
   logHandler
 }
